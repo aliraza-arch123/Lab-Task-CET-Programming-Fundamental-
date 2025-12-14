871b296ee36e0b52d56e20d71d7a408a8921c3a1
Ali Raza <alimughal12921292@gmail.com>
2025-12-14 23:05:26 +0500

    Add files via upload

---
+++ Task 4/Task 15.(Traffic Signal)Write a program that simulates a traffic signal system.The user enters the color of the signal as R (Red) G (Green) or Y (Yellow) and the program.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  char c ;
+	cout<<"Enter the colour:";
+	cin>>c;
+	if(c== 'R'||c=='r')
+	cout<<"stop"<<endl;
+	if(c=='G'|| c=='g')
+	cout<<"go"<<endl;
+	if(c=='Y'|| c=='y')
+	cout<<"slow down"<<endl;
+	system("pause");
+	return 0;
+}
