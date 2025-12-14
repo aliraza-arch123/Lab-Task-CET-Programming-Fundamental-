8efb1864f076fbb50dcc1ba92fd016eecd8adb45
Ali Raza <alimughal12921292@gmail.com>
2025-12-14 23:00:47 +0500

    Add files via upload

---
+++ Task 2/Task 7. Average of predefined Test Scores Rounded to two Decimal places..cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    
+    double score1 = 85.5;
+    double score2 = 90.75;
+    double score3 = 88.25;
+
+    
+    double average = (score1 + score2 + score3) / 3.0;
+
+    
+    cout << "The average of " << score1 << ", " << score2 << ", and " << score3 << " is: ";
+    cout << std::fixed << std::setprecision(2) << average << std::endl;
+    system("pause");
+    return 0;
+}
