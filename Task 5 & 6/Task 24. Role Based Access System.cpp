657fc68a20cf1e33b748110f58882a6ee5dedeef
Ali Raza <alimughal12921292@gmail.com>
2026-01-23 01:39:19 +0500

    Add files via upload

---
+++ Task 5 & 6/Task 24. Role Based Access System.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string username, password, role;
+
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
+
+    if (username == "admin" && password == "password123") {
+        cout << "Authentication Successful!" << endl;
+
+        cout << "Enter your role: ";
+        cin >> role;
+
+        if (role == "Admin") {
+            cout << "Full Access" << endl;
+        } else if (role == "Guest") {
+            cout << "Limited Access" << endl;
+        } else {
+            cout << "No Access" << endl;
+        }
+    } else {
+        cout << "Authentication Failed. Access Denied." << endl;
+    }
+
+    return 0;
+}
+
