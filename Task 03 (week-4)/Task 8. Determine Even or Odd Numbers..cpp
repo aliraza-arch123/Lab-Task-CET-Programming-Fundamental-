9fe78b2a81a4fff10cebf83e512aff1d33a3ed64
Ali Raza <alimughal12921292@gmail.com>
2026-01-23 02:24:18 +0500

    Add files via upload

---
+++ Task 03 (week-4)/Task 8. Determine Even or Odd Numbers..cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    string result = (num % 2 == 0) ? "even" : "odd";
+
+    cout << "The number " << num << " is " << result << endl;
+    system("pause");
+    return 0;
+}
